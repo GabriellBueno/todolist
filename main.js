53845e01ccc825f3c0871be901ff52a981b3678d
Gabriel Bueno <gabrielbueno.pgr@gmail.com>
2024-01-23 00:44:53 -0300

    Concluido

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
-$(document).ready(function () {
+$(document).ready(function(){
     $('form').on('submit', function (refresh_off) {
         refresh_off.preventDefault()
 
-        let input_item = $('.input-afazeres').val()
-        let input_descricao = $('.input-descricao').val()
+        let afazeres = $('.input-afazeres').val()
+        let descricao = $('.input-descricao').val()
 
-        let novo_item = $(`<li style="display: none"></li>`)
+        let novo_afazer = $('<li style="display: none"></li>')
 
         $(`
-        <div class="container">
-            <div class="task" id="box">
-            <button class="okay" id="botao-verde" type="button"><img class="okay" src="images/Okay.png"></button>
-            <h2 class="titulo-tarefa">${input_item}</h3>
-            <p class="titulo-tarefa">${input_descricao}</p>
-            <button class="excluir" id="botao-excluir" type="button"><img class="excluir" src="images/Trash bin.png"></button>
-            </div>`).appendTo(novo_item)
+                <div class="task" id="box">
+                <button type="button" class="okay" id="botao-okay"><img class="okay" src="images/Okay.png" alt=""></button>
+                <h2 class="titulo-tarefa">${afazeres}</h2>
+                <p class="titulo-tarefa">${descricao}</p>
+                <button type="button" class="excluir" id="botao-excluir"><img class="excluir" src="images/Trash bin.png" alt=""></button>
+                </div>
+        `).appendTo(novo_afazer)
 
-        $(novo_item).appendTo('ul')
-        $(novo_item).fadeIn(500)
-
-        $('.input-afazeres').val('')
-        $('.input-descricao').val('')
+        $(novo_afazer).appendTo('ul')
+        $(novo_afazer).fadeIn(500)
     })
-    $('ul').on('click', '#botao-excluir', function () {
-        $(this).closest('li').fadeOut(function () {
-            $(this).remove()
-        })
+    $('ul').on('click', '#botao-okay', function(){
+        $(this).closest('li').find('#box').css('background-color' , '#1ed687')
+        $(this).closest('li').find('#box').css('text-decoration' , 'line-through')
     })
-    $('ul').on('click', '#botao-verde', function () {
-        $(this).closest('li').fadeIn(function () {
-            $('.task').css("background-color", "#1ed687")
-        })
+    $('ul').on('click', '#botao-excluir', function(){
+        $(this).closest('li').fadeOut(500)
     })
 })
